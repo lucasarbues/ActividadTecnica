8fd13a827da666d4a0495626796e3f3274754d16
lucasarbues <lucasarbues76@gmail.com>
2023-05-22 15:22:31 -0300

    contrato wft

---
+++ WFToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WFToken is ERC20{
+    constructor() ERC20("WFToken", "WFT"){
+        _mint(msg.sender,1000*10**18);
+    }
+}
